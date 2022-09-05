473b0ec233314555951db3260874676abb04df4b
NikaLuki <lukinykh.nikita@yandex.ru>
2022-09-06 01:48:51 +0300

    fixed hide table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -18,7 +18,7 @@ const Users = () => {
     else if (number < 5 && number > 1)
       return (
         <span className="badge bg-primary fs-3">
-          {number + ' человека тусанет с тобой сегодня'}
+          {number + ' человека тусанут с тобой сегодня'}
         </span>
       );
     else
@@ -31,52 +31,56 @@ const Users = () => {
   return (
     <>
       {renderPhrase(users.length)}
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качество</th>
-            <th scope="col">Проффессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => {
-            return (
-              <tr key={user._id}>
-                <td>{user.name} </td>
-                <td>
-                  {user.qualities.map(qualitie => {
-                    return (
-                      <span
-                        key={qualitie._id}
-                        className={'badge bg-' + qualitie.color + ' m-1'}
-                      >
-                        {qualitie.name}
-                      </span>
-                    );
-                  })}
-                </td>
-                <td>{user.profession.name}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{user.rate + '/5'}</td>
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => {
-                      handleDelete(user._id);
-                    }}
-                  >
-                    delete
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {users.length > 0 ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качество</th>
+              <th scope="col">Проффессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => {
+              return (
+                <tr key={user._id}>
+                  <td>{user.name} </td>
+                  <td>
+                    {user.qualities.map(qualitie => {
+                      return (
+                        <span
+                          key={qualitie._id}
+                          className={'badge bg-' + qualitie.color + ' m-1'}
+                        >
+                          {qualitie.name}
+                        </span>
+                      );
+                    })}
+                  </td>
+                  <td>{user.profession.name}</td>
+                  <td>{user.completedMeetings}</td>
+                  <td>{user.rate + '/5'}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => {
+                        handleDelete(user._id);
+                      }}
+                    >
+                      delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
